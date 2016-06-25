23665fcd14ec4f701ffc0d8fc724b5a4d902b14a
catkhuu <sunshine.dumbo@gmail.com>
2016-06-25 17:23:05 -0400

    DECLARED array and hashed array. Filled both with information from building directory.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -11,3 +11,12 @@ def doggy_treats
 end
 
 doggy_treats {|name| puts "Bark! Bark! Me! #{name}. I want treats!"}
+
+# Use .each, .map, and .map!
+# Declare an array and a hash, and populate each of them with some data.
+
+main_floors_in_building = [3, 8, 11, 12, 13, 14, 18]
+
+building_directory = {3: ["Juvenile", "Southwest"], 8: ["DDOU", "PNTF", "Tech"], 11: ["Cental", "East", "Northeast", "Northwest", "South"], 12: ["FVSA", "Homicide", "Victim Services"], 13: ["Appeals", "Legal"], 14: ["Pre-Trial"], 18: ["ECCU", "Government Fraud", "Insurance Fraud", "Special Investigations", "Community Outreach"]}
+
+
