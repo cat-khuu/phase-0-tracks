f1e449d69381749496227388883e8a506fa400a5
catkhuu <sunshine.dumbo@gmail.com>
2016-06-26 11:41:07 -0400

    ADDED pseudocode for user interface.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,6 +10,8 @@
 # Substitute all vowels [aeiou] in full_name with the next vowel [eioua]. Substitute all consonants in full_name with the next consonant in the alphabet. Store result in full_name_id.
 # Convert full_name_id back into array. Iterate through the array and capitalized the first letter in the first and last name. Convert the array back into a string. Store result in full_name_idx.
 
+alias_history = {}
+
 def alias_manager(covert_spy_name)
   arrayed_name = covert_spy_name.downcase.split(" ")
   full_name = arrayed_name.reverse.join(" ")
@@ -17,4 +19,27 @@ def alias_manager(covert_spy_name)
   full_name_idx = full_name_id.split.map {|x| x.capitalize}.join(' ')
 end
 
-p alias_manager("Felicia Torres")
+# <--- commented out driver code for alias_manager --->
+# p alias_manager("Felicia Torres")
+
+
+# Provide a user interface that lets a user enter a name and get a fake name back. Let the user do this repeatedly until they decide to quit by typing 'quit'. (They might just hit Enter to continue.)
+
+# Create empty hash to store given spy name(key) and new spy name (value). Name hash alias_history
+# Prompt user for name
+# Given name = covert_spy_name
+# Call alias_manager. Pass covert_spy_name as argument.
+# Print result from alias_manager
+# Store result in alias_history, where covert_spy_name(given name) will be the key, and the result of alias_manager will be the value.
+# Ask user if they are satisfied with their new spy name.
+# - BEGIN LOOP
+# IF "n", then ask user to enter another spy name.
+# - Call alias_manager. Pass covert_spy_name as argument.
+# - Print result from alias manager.
+# ELSIF "y"
+# - Print "Type 'quit' to exit and print your alias history."
+# - Store user input in variable answer.
+# ELSE print "I'm sorry, I didn't get that..."
+# - DO UNTIL answer == "quit" to exit program and print alias history
+# PRINT alias_history
+
