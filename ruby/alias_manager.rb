12be05cbcb710836e0c1862244d47a399111117c
catkhuu <sunshine.dumbo@gmail.com>
2016-06-26 10:42:34 -0400

    ADDED pseudocode for alias_manager.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+# Write a method that takes a spy's real name (e.g. "Felicia Torres") and creates a fake name with it by doing the following:
+# Swapping the first and last name
+# Changing all the vowels (a,e,i,o,u) to the next vowel in 'aeiou', and all of the consonants to the next consonants in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+# "Felicia Torres" should return "Vussit Gimodoe"
+
+# Take given name (e.g. "Felicia Torres")
+# Convert letters to lowercase and convert string to array. Store result in arrayed_name. "Felicia Torres" => ["felicia", "torres"]
+# Swap positions of the first and last name in arrayed_name.
+# Convert array back into string of full name. Store result in full_name. This should return: ["torres", "felicia"] => "torres felicia"
+# Substitute all vowels [aeiou] in full_name with the next vowel [eioua]. Substitute all consonants in full_name with the next consonant in the alphabet. Store result in full_name_id.
+# Convert full_name_id back into array. Iterate through the array and capitalized the first letter in the first and last name. Convert the array back into a string. Store result in full_name_idx.
+
+
