ed324c87f5455a9ee89b53e7ef3b722ce1ce17d9
catkhuu <sunshine.dumbo@gmail.com>
2016-06-27 07:08:50 -0400

    ADDED pseudocode for user input.

---
+++ ruby/hashes.rb
@@ -0,0 +1,9 @@
+# Your program should ...
+
+# PROMPT the designer/user for all of this information.
+# CONVERT any user input to the appropriate data type.
+# - convert user input for 'age' to an integer
+# - convert user input for 'dogs_play_poker' and 'trump_tower' to booleans (t/f)
+# INITIATE empty hash and label as 'result'
+# STORE user input as keys(category) and values (user response) in result
+# PRINT result after the user has answered all of the questions.
