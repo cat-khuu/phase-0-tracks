7fec75dc4ea55d85ad363b312f86e101d4e1945e
catkhuu <sunshine.dumbo@gmail.com>
2016-06-27 07:25:58 -0400

    ADDED case for edit_key, when 'y' and when 'n'.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -52,3 +52,47 @@ puts "Here are your preferences, thus far: #{result}."
 # WHEN 'N'
 # - Print a thank you message and tell the user you will create the #{decor_theme} that they have always envisioned.
 # - If the user enters a response (edit_key) that is other than 'y' or 'n', then print "I'm sorry, I didn't get that..."
+
+keys = result.keys
+
+puts "Do you want to edit any of your responses? (y/n)"
+edit_key = gets.chomp
+
+case edit_key
+when "y"
+  puts "Enter the name of the category you want to make changes to. Choose from: #{keys.join(", ")}."
+  changed_key = gets.chomp.to_sym
+    if changed_key == :name
+      puts "What is your name?"
+      name = gets.chomp
+      result[:name] = name
+    elsif changed_key == :age
+      puts "How old are you?"
+      age = gets.to_i
+      result[:age] = age
+    elsif changed_key == :offspring
+      puts "How many offspring do you have?"
+      offspring = gets.to_i
+      result[:offspring] = offspring
+    elsif changed_key == :decor_theme
+      puts "How would you define your decor style now?"
+      decor_theme = gets.chomp
+      result[:decor_theme] = decor_theme
+    elsif changed_key == :likes_dogs
+      puts "Dislike the idea of a 'dogs playing poker' theme? (y/n)"
+      dogs_play_poker = gets.chomp
+      likes_dogs = dogs_play_poker == "n"
+      result[:likes_dogs] = likes_dogs
+    elsif changed_key == :trump_fanatic
+      puts "Love the thought of your home being dubbed as 'The Trump Tower 2.0'? (y/n)"
+      trump_tower = gets.chomp
+      trump_fanatic = trump_tower == "y"
+      result[:trump_fanatic] = trump_fanatic
+    else
+      puts "That is not an option"
+    end
+when "n"
+  puts "Thank you for your input. We will have an interior designer dispatched to your home shortly to create your envisioned dream of #{result[:decor_theme]}."
+else
+  puts "I didn't get that..."
+end
