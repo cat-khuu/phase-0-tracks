26e679f9d2ef59f90d5ed64d1572cfd199107888
catkhuu <sunshine.dumbo@gmail.com>
2016-06-27 07:10:25 -0400

    ADDED user interface for user input.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,3 +7,21 @@
 # INITIATE empty hash and label as 'result'
 # STORE user input as keys(category) and values (user response) in result
 # PRINT result after the user has answered all of the questions.
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.to_i
+
+puts "How many offspring do you have?"
+offspring = gets.to_i
+
+puts "How would you define your decor style?"
+decor_theme = gets.chomp
+
+puts "Dislike the idea of a 'dogs playing poker' theme? (y/n)"
+dogs_play_poker = gets.chomp
+
+puts "Love the thought of your home being dubbed as 'The Trump Tower 2.0'? (y/n)"
+trump_tower = gets.chomp
