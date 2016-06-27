58c9d76bca5404a292d309f6092a66992cdf35b2
catkhuu <sunshine.dumbo@gmail.com>
2016-06-27 07:24:25 -0400

    ADDED pseudocode for user to update value in hash.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -37,3 +37,18 @@ result[:likes_dogs] = likes_dogs
 result[:trump_fanatic] = trump_fanatic
 
 puts "Here are your preferences, thus far: #{result}."
+
+# Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+
+# Separate keys from result to display which categories the user can edit their responses for.
+# Ask user if they want to edit any of their responses
+# If yes, then write case statement for when 'y' and when 'n'
+# WHEN 'Y'
+# - Prompt user to enter category (key) they want to make changes to.
+# - Convert response to a symbol ("age" => :age) and store in variable changed_key
+# - Depending on what key they enter, the user will be asked the question related to the key. (e.g. if changed_key == :age, then the user will be asked: "How old are you?")
+# - Store updated value in result
+# - If user enters a key that is not an option, then print "That is not an option."
+# WHEN 'N'
+# - Print a thank you message and tell the user you will create the #{decor_theme} that they have always envisioned.
+# - If the user enters a response (edit_key) that is other than 'y' or 'n', then print "I'm sorry, I didn't get that..."
