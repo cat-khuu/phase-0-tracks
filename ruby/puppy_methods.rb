0876e85840c3f4d38f38ae3886cf08ff5275be90
catkhuu <sunshine.dumbo@gmail.com>
2016-06-28 09:42:55 -0400

    ADDED pseudocode for Puppy#dog_years.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -16,6 +16,19 @@ class Puppy
     puts "*rolls over*"
   end
 
+# Puppy#dog_years
+# input: integer as human years
+# output: integer as dog years
+# Conversion:
+# - 1 human year = 15 dog years
+# - 2 human years = 24 dog years
+# 3 years and up = +4 years to 2 human years(24)
+# IF human_years equals 1, then dog_age = 15
+# ELSIF human_years equals 2, then dog_age = 24
+# ELSIF human_years is equal to or greater than 3, then dog_age = 24 + (4 * (human_years-2)). Suppose human_years = 3. dog_age = 24 + (4 * (3-2)) = 28.
+# ELSE print "Invalid input. Please enter an integer."
+# RETURN dog_age
+
 
 end
 
