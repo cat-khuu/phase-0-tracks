0ffee00664b797c746c85190c4230d0778dbbbd4
catkhuu <sunshine.dumbo@gmail.com>
2016-07-04 15:00:17 -0400

    Set likes_the_philadelphia_eagles as an instance variable passed on in initialization.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -12,12 +12,16 @@
 # ethnicity, which will be a string passed in on initialization**
 # reindeer_ranking, an array of reindeer from most preferred to least preferred. This is NOT passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 # age, which is NOT passed in on initialization and defaults to 0
+# ------------------------------------------ #
+
+# Add some diverse initializations to your file as driver code, then test your work by running the program from the command line.
 
 class Santa
-  def initialize(gender, ethnicity)
+  def initialize(gender, ethnicity, likes_the_philadelphia_eagles)
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
+    @likes_the_philadelphia_eagles = likes_the_philadelphia_eagles
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
