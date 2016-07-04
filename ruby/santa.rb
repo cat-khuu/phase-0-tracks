af5ad9a61af9f3b2b9d715f00cee697b8160370b
catkhuu <sunshine.dumbo@gmail.com>
2016-07-04 14:46:49 -0400

    Definited initialize method.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,2 +1,13 @@
+# Your class should have three instance methods:
+
+# A speak method that will print "Ho, ho, ho! Haaaappy holidays!"
+# An eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!"
+# An initialize method that prints "Initializing Santa instance ...".
+# Driver code
+
 class Santa
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
 end
