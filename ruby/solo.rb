e450013ce4b9bd4d48c20aaa6a1914f6d5b33fea
catkhuu <sunshine.dumbo@gmail.com>
2016-07-05 12:01:14 -0400

    Added pseudocode for class design as comments.

---
+++ ruby/solo.rb
@@ -0,0 +1,17 @@
+# Design a class. Your class should have:
+# - at least three attributes (using at least two data types) wholes values will vary from instance to instance
+# - at least three methods, at least one of which takes an argument
+
+# POLITICIAN class
+
+# ATTRIBUTES - passed during initialization
+# age - integer
+# name - string
+# political party - string
+
+# METHODS
+# kiss_baby - "*kisses supporter's baby*"
+# smile - "*smiles for photo*"
+# fundraise(promise) - "If you vote for me, I promise to #{promise}." return promise.
+
+
