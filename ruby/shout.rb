1da28e64224cacf9c95a8a4d8e433d0ce5a84bf8
catkhuu <sunshine.dumbo@gmail.com>
2016-07-06 16:29:40 -0400

    Removed 'self' from yell_angrily and yelling_angrily methods.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,11 @@
 # methods for standalone modules (NOT mixins) must be defined on the 'self' keyword.
 
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words.upcase + "!!!" + " :D :D :D"
   end
 end
