e59a07262cbbbe79c05e460301501c3f2f836115
catkhuu <sunshine.dumbo@gmail.com>
2016-07-06 16:29:05 -0400

    Added driver code to test methods on both classes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -20,3 +20,11 @@ end
 class Motorist
   include Shout
 end
+
+pedestrian = Pedestrian.new
+p pedestrian.yell_angrily("Watch where you're going! I'm walking here")
+p pedestrian.yelling_happily("another day, another street crossed without getting run over")
+
+motorist = Motorist.new
+p motorist.yell_angrily("You mean, I went through the wrong toll?!?")
+p motorist.yelling_happily("traffic court abolished. take that, PPA")
