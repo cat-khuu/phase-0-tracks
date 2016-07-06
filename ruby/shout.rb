ff7560095248e5c707e0eca76188bdeff6653097
catkhuu <sunshine.dumbo@gmail.com>
2016-07-06 15:57:57 -0400

    Defined method for yelling happily.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,11 @@
+# methods for standalone modules (NOT mixins) must be defined on the 'self' keyword.
+
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words.upcase + "!!!" = " :D :D :D"
   end
 end
