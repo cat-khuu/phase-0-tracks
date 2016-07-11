f78c338c3c888f64c815db043bd2d2a356d64063
catkhuu <sunshine.dumbo@gmail.com>
2016-07-11 17:08:13 -0400

    Added feature to run virus_effects on all 50 states. Refactored virus_effects.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,8 +4,8 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# require - loadpath is known
+# require_relative  - it is your own project and you know where the file is located within your project
 require_relative 'state_data'
 
 class VirusPredictor
@@ -16,22 +16,26 @@ class VirusPredictor
     @population_density = population_density
   end
 
+# calls both methods - predicted_deaths and speed_of_spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+# number of deaths for each state depending on population density and population. The greater the population density then the greater the number of deaths.
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
+
+    case @population_density
+    when @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
+    when @population_density >= 150
       number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
+    when @population_density >= 100
       number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
+    when @population_density >= 50
       number_of_deaths = (@population * 0.1).floor
     else
       number_of_deaths = (@population * 0.05).floor
@@ -41,18 +45,20 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# how quickly the virus will spread. The greater the population density then the quicker the virus will spread.
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
+    case @population_density
+    when @population_density >= 200
       speed += 0.5
-    elsif @population_density >= 150
+    when @population_density >= 150
       speed += 1
-    elsif @population_density >= 100
+    when @population_density >= 100
       speed += 1.5
-    elsif @population_density >= 50
+    when @population_density >= 50
       speed += 2
     else
       speed += 2.5
@@ -70,17 +76,22 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+STATE_DATA.each do |state, population|
+  state_demographics = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
+  state_demographics.virus_effects
+end
 
 
 #=======================================================================
