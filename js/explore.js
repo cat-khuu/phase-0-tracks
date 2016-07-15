155c0a3c7754fa7120490003b4a5f71d80131619
catkhuu <sunshine.dumbo@gmail.com>
2016-07-15 14:04:52 -0400

    Added pseudocode for reverse string feature.

---
+++ js/explore.js
@@ -0,0 +1,11 @@
+// reverse string
+// input: string
+// output: reverse string
+// IF var str is a string (both in type and value) OR as an instance of a string, then print reverse string.
+//  REVERSE STRING:
+//   - take string and convert to array using .split
+//   - reverse array using .reverse
+//   - convert array back into a string using .join
+//   - store result of reversed string in new variable reverseStr
+//   - return reverseStr
+// ELSE if var str is not a string, then print to console var notAStr. var notAStr = "Error: inputted value not a string. Please input a string. (Ex. 'hello')"
