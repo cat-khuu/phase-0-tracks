441ffa0e346106ac98a456e9264345915993942d
catkhuu <sunshine.dumbo@gmail.com>
2016-07-15 14:19:21 -0400

    Defined reverseStr method.

--- js/explore.js
+++ js/explore.js
@@ -3,9 +3,20 @@
 // output: reverse string
 // IF var str is a string (both in type and value) OR as an instance of a string, then print reverse string.
 //  REVERSE STRING:
-//   - take string and convert to array using .split
+//   - take string and convert to array using.split
 //   - reverse array using .reverse
-//   - convert array back into a string using .join
-//   - store result of reversed string in new variable reverseStr
-//   - return reverseStr
-// ELSE if var str is not a string, then print to console var notAStr. var notAStr = "Error: inputted value not a string. Please input a string. (Ex. 'hello')"
+//   - convert array back into a string using.join
+//   - return the result (reversed string).
+// ELSE if var str is not a string, then print to console and return : "Error: inputted value not a string. Please input a string. (Ex. 'hello')"
+
+function reverseStr(str) {
+  if (typeof str === 'string' || str instanceof String)
+    // it's a string
+    return str.split('').reverse().join('');
+  else
+  // it's not a string
+    notAStr =  console.log("Error: inputted value not a string. Please input a string. (Ex.'hello')");
+    return notAStr;
+
+}
+
