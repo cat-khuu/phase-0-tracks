6d48ca40843ccfae74f2b4667c5e2c4ab0a6f6cd
catkhuu <sunshine.dumbo@gmail.com>
2016-07-16 09:20:01 -0400

    Created new object: horseDescriptions, from both arrays.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,13 +1,3 @@
-var horse_colors = ["blue sparkles", "purple zebra", "hot pink", "silver rainbows"];
-
-var horse_names = ["Bumblebee", "Glitter", "Zoot", "Cherub"];
-
-horse_colors.push("blue clouds");
-horse_names.push("Halo");
-
-console.log(horse_colors);
-console.log(horse_names);
-
 // Pseudocode to create an object with both arrays. Where, the horse name will be the key for the object, and the horse color will be the value.
 // Input: horse_colors and horse_names - arrays
 // Output: horseDescriptions - object
@@ -16,4 +6,25 @@ console.log(horse_names);
 // - while index = 0, index < horse_names.length, index +=1 do...
 // - horseDescriptions[horse_names[index]] = horse_colors[index];
 // print horseDescriptions
+// ----------------------------------------- //
 
+// define two arrays - one with horse colors and the other with horse names
+var horse_colors = ["blue sparkles", "purple zebra", "hot pink", "silver rainbows"];
+
+var horse_names = ["Bumblebee", "Glitter", "Zoot", "Cherub"];
+
+// add an item to each array
+horse_colors.push("blue clouds");
+horse_names.push("Halo");
+
+// print each array to show the item was added
+console.log(horse_colors);
+console.log(horse_names);
+
+// create an object from both arrays. Where, the key for the object will be the name, and the value will be the color.
+var horseDescriptions = {};
+for (var index = 0; index < horse_names.length; index +=1) {
+  horseDescriptions[horse_names[index]] = horse_colors[index];
+}
+
+console.log(horseDescriptions);
