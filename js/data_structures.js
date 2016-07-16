e50933d0d35efd8173a37332b37e42f1b75019a2
catkhuu <sunshine.dumbo@gmail.com>
2016-07-16 09:14:04 -0400

    Pseudocoded to create object from two arrays.

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,13 @@ horse_names.push("Halo");
 
 console.log(horse_colors);
 console.log(horse_names);
+
+// Pseudocode to create an object with both arrays. Where, the horse name will be the key for the object, and the horse color will be the value.
+// Input: horse_colors and horse_names - arrays
+// Output: horseDescriptions - object
+// Define empty object as horseDescriptions.
+// Create loop to loop through horse_names. So:
+// - while index = 0, index < horse_names.length, index +=1 do...
+// - horseDescriptions[horse_names[index]] = horse_colors[index];
+// print horseDescriptions
+
