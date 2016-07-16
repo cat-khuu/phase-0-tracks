d5cdc1c7a267b5376d218d9e5acea3801fa17e05
catkhuu <sunshine.dumbo@gmail.com>
2016-07-16 08:50:34 -0400

    Added a color and another name to each respective array.

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,3 +2,8 @@ var horse_colors = ["blue sparkles", "purple zebra", "hot pink", "silver rainbow
 
 var horse_names = ["Bumblebee", "Glitter", "Zoot", "Cherub"];
 
+horse_colors.push("blue clouds");
+horse_names.push("Halo");
+
+console.log(horse_colors);
+console.log(horse_names);
