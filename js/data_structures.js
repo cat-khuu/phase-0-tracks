6fd1e73eb71a0d28aa4e2555780df35e0054283d
catkhuu <sunshine.dumbo@gmail.com>
2016-07-16 09:34:02 -0400

    Fixed formatting. Fixed wording in comments.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,14 +1,14 @@
 // Pseudocode to create an object with both arrays. Where, the horse name will be the key for the object, and the horse color will be the value.
 // Input: horse_colors and horse_names - arrays
 // Output: horseDescriptions - object
-// Define empty object as horseDescriptions.
+// Declare empty object as horseDescriptions.
 // Create loop to loop through horse_names. So:
 // - while index = 0, index < horse_names.length, index +=1 do...
 // - horseDescriptions[horse_names[index]] = horse_colors[index];
 // print horseDescriptions
 // ----------------------------------------- //
 
-// define two arrays - one with horse colors and the other with horse names
+// declare two arrays - one with horse colors and the other with horse names
 var horse_colors = ["blue sparkles", "purple zebra", "hot pink", "silver rainbows"];
 
 var horse_names = ["Bumblebee", "Glitter", "Zoot", "Cherub"];
