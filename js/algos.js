a0bef122c0271f8843a814e8dcfb8744d372ef95
catkhuu <sunshine.dumbo@gmail.com>
2016-07-16 14:13:13 -0400

    Added pseodocode for release 0 as comments.

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// write a function that takes an array of words or phrases and returns the longest word or phrase in the array. Example: ["long phrase", "longest phrase", "longer phrase"] => "longest phrase"
+// input: array of strings
+// output: longest string or phrase
+// sort the array in descending order from longest string to shorteset string.
+// - use built-in function array.sort()
+// - write function to evaluate b.length - a.length, and to return result of b - a.
+// return the string at index[0] in the array.
+
