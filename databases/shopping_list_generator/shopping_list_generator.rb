a38fae8f7ad93cab88c40fed84e9a1f32d66f7ca
catkhuu <sunshine.dumbo@gmail.com>
2016-07-25 11:01:31 -0400

    Created tables for 4 seasons and shopping_list.

--- databases/shopping_list_generator/shopping_list_generator.rb
+++ databases/shopping_list_generator/shopping_list_generator.rb
@@ -1,3 +1,14 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new("shopping_list_generator.db")
+
+# create tables for each season: Spring, Summer, Fall, Winter
+db.execute("CREATE TABLE IF NOT EXISTS Spring(spring_id INTEGER PRIMARY KEY, spring_veg_name VARCHAR(255) UNIQUE, March BOOLEAN, April BOOLEAN, May BOOLEAN)")
+
+db.execute("CREATE TABLE IF NOT EXISTS Summer(summer_id INTEGER PRIMARY KEY, summer_veg_name VARCHAR(255) UNIQUE, June BOOLEAN, July BOOLEAN, August BOOLEAN)")
+
+db.execute("CREATE TABLE IF NOT EXISTS Fall(fall_id INTEGER PRIMARY KEY, fall_veg_name VARCHAR(255) UNIQUE, September BOOLEAN, October BOOLEAN, November BOOLEAN)")
+
+db.execute("CREATE TABLE IF NOT EXISTS Winter(winter_id INTEGER PRIMARY KEY, winter_veg_name VARCHAR(255) UNIQUE, December BOOLEAN, January BOOLEAN, February BOOLEAN)")
+
+db.execute("CREATE TABLE IF NOT EXISTS Shopping_list(list_id INTEGER PRIMARY KEY, item_name VARCHAR(255) UNIQUE")
